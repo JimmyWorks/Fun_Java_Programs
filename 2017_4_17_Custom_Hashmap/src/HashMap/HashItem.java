c96816808e46008a34d8c9d5ee3ee3111c83b5d5
Jimmy Nguyen <jimmy@jimmyworks.net>
2017-04-19 13:43:38 -0500

    Java - Custom Hashmap with Quadratic Probing
    [4/18/2017] Java program exploring the creation of a custom HashMap object storing HashItem objects. The HashMap uses a hash table initialized with a size of 9 and 50% loading. Quadratic probing is implemented for collisions and the driver program tests the full functionality when inserting hash items, collisions and multiple collisions, rehashing, and collision wrap-around.

---
+++ 2017_4_17_Custom_Hashmap/src/HashMap/HashItem.java
@@ -0,0 +1,18 @@
+/*
+ * CS 2336 Computer Science II
+ * Homework 7: Custom Quadratic Hash Table
+ * Author: Jimmy Nguyen
+ * Contact me: Jimmy@JimmyWorks.net
+ */
+package HashMap;
+
+
+public class HashItem {
+    public int key;
+    public String name;
+    
+    public HashItem(int key, String name){
+        this.key = key;
+        this.name = name;
+    }
+}
---
+++ 2017_4_17_Custom_Hashmap/src/Main.java
@@ -0,0 +1,83 @@
+/*
+ * CS 2336 Computer Science II
+ * Homework 7: Custom Quadratic Hash Table
+ * Author: Jimmy Nguyen
+ * Contact me: Jimmy@JimmyWorks.net
+ */
+
+import HashMap.*;
+
+public class Main {
+    
+    static final int[] TEST_NUMBERS = {101, 345, 603, 612, 621, 354, 900, 99, 197};
+    static final String[] TEST_VALUES = {   "one hundred one",
+                                            "three hundred five",
+                                            "six hundred three",
+                                            "six hundred twelve",
+                                            "six hundred twenty-one",
+                                            "three hundred fifty-four",
+                                            "nine hundred",
+                                            "ninety-nine",
+                                            "one hundred ninety-seven"};
+        
+    public static void main(String[] args) {
+        System.out.println("Let's make an array of items we can use test the custom HashMap!\n");
+        System.out.println("Creating array of hash items...\n");
+        HashItem[] testArray = new HashItem[TEST_NUMBERS.length];
+        
+        for(int i = 0; i < TEST_NUMBERS.length; i++){
+            testArray[i] = new HashItem(TEST_NUMBERS[i], TEST_VALUES[i]);
+        }
+                
+        System.out.println("Here are the items in our array");
+        for(HashItem i: testArray){
+            System.out.printf("Key: %d, String Member: %s\n", i.key, i.name);
+        }
+        System.out.println("\n\n==========================================================\n");
+        System.out.println("Okay, now to create the HashMap.  The default size is 9 with a loading of 50%\n");
+        System.out.println("Creating Hashmap...\n");
+        
+        HashMap myHashmap = new HashMap();
+        
+        System.out.printf("HashMap created.  Number of entries: %d\n\n", myHashmap.sizeOf());
+        
+        System.out.println("Let's add the following items from our array into the HashMap:");
+        System.out.println("101, 345, 603, 612\n");
+        
+        myHashmap.insert(testArray[0]);
+        myHashmap.insert(testArray[1]);
+        myHashmap.insert(testArray[2]);
+        myHashmap.insert(testArray[3]);
+        
+        System.out.println("Current HashMap...");
+        myHashmap.print();
+        
+        System.out.println("\n\n==========================================================\n");
+        System.out.println("The next entry should cause the loading to exceed the limit of 0.5!\nNow inserting 99\n");
+        
+        myHashmap.insert(testArray[7]);
+        
+        System.out.println("First rehash complete.  Current HashMap...");
+        myHashmap.print();
+        
+        System.out.println("\n\n==========================================================\n");
+        System.out.println("Finally, let's test wrap-around entries\nNow inserting entries: 197, 197\n");
+        
+        myHashmap.insert(testArray[8]);
+        myHashmap.insert(testArray[8]);
+        
+        System.out.println("Current HashMap...");
+        myHashmap.print();
+        
+        System.out.println("\n\n==========================================================\n");
+        System.out.println("Let's insert a few more values to see a second rehash.\nNow inserting 354, 900\n");
+    
+        myHashmap.insert(testArray[5]);
+        myHashmap.insert(testArray[6]);
+        
+        System.out.println("Second rehash complete.  Current HashMap...");
+        myHashmap.print();
+    }
+
+        
+}
